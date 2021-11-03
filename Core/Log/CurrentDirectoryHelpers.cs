7485164a01d40fae4a0bdd2060cc12bf8ed50874
Marcus Costa <marcusviniciusdefreitascosta@yahoo.com.br>
2021-11-02 22:28:38 -0300

    Configurando padrão para logs e adicinada classe helper.

---
+++ Core/Log/CurrentDirectoryHelpers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using System.Runtime.InteropServices;
+
+namespace Core.Log
+{
+    public class CurrentDirectoryHelpers
+    {
+        internal const string AspNetCoreModuleDll = "aspnetcorev2_inprocess.dll";
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport(AspNetCoreModuleDll)]
+        private static extern int http_get_application_properties(ref IISConfigurationData iiConfigData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct IISConfigurationData
+        {
+            public IntPtr pNativeApplication;
+
+            [MarshalAs(UnmanagedType.BStr)]
+            public string pwzFullApplicationPath;
+
+            [MarshalAs(UnmanagedType.BStr)]
+            public string pwzVirtualApplicationPath;
+
+            public bool fWindowsAuthEnabled;
+            public bool fBasicAuthEnabled;
+            public bool fAnonymousAuthEnable;
+        }
+
+        public static void SetCurrentDirectory()
+        {
+            try
+            {
+                // Check if physical path was provided by ANCM
+                var sitePhysicalPath = Environment.GetEnvironmentVariable("ASPNETCORE_IIS_PHYSICAL_PATH");
+                if (string.IsNullOrEmpty(sitePhysicalPath))
+                {
+                    // Skip if not running ANCM InProcess
+                    if (GetModuleHandle(AspNetCoreModuleDll) == IntPtr.Zero)
+                    {
+                        return;
+                    }
+
+                    IISConfigurationData configurationData = default(IISConfigurationData);
+                    if (http_get_application_properties(ref configurationData) != 0)
+                    {
+                        return;
+                    }
+
+                    sitePhysicalPath = configurationData.pwzFullApplicationPath;
+                }
+
+                Environment.CurrentDirectory = sitePhysicalPath;
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+    }
+}
